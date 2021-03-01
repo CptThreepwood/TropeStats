4bfc16e0a828c1c593bde2c1f3bbede4713e7ac4
David Jennens <d.jennens@gmail.com>
2021-03-01 06:49:28 +0000

    Change project structure

---
+++ scraping/httrack_cmd.sh
@@ -0,0 +1,1 @@
+httrack https://tvtropes.org/pmwiki/index_report.php https://tvtropes.org/pmwiki/pmwiki.php/Main/Media https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?n=Main&t=trope -W -O ./raw/TvTropes -i -%v -x -d -#L1000000 -https://tvtropes.org/* +https://tvtropes.org/pmwiki/pmwiki.php/* -*?*
