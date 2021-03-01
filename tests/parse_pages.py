b6153adb3037df823d33f42207f2d8020f35f927
David Jennens <d.jennens@gmail.com>
2021-03-02 00:02:49 +1100

    One last project restructure

--- tests/parse_pages.py
+++ tests/parse_pages.py
@@ -3,8 +3,8 @@ import unittest
 
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-import parsing.parse_html
-parsing.parse_html.HTML_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
+from src.parsing import parse_html
+parse_html.HTML_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 class TestParsing(unittest.TestCase):
 
@@ -12,38 +12,29 @@ class TestParsing(unittest.TestCase):
         '''
         Parsing a Media Page
         '''
-        links = scraping.parse_media_for_tropes.get_links('Anime/AceAttorney')
+        links = parse_html.get_links('Anime/AceAttorney')
         self.assertEqual(len(links), 174)
 
     def test_media_with_subpages(self):
         '''
         Parsing a Media Page with subpages
         '''
-        links = scraping.parse_media_for_tropes.get_links('WesternAnimation/AvatarTheLastAirbender')
+        links = parse_html.get_links('WesternAnimation/AvatarTheLastAirbender')
         self.assertEqual(len(links), 1337)
 
     def test_trope(self):
         '''
         Parsing a Media Page
         '''
-        links = scraping.parse_media_for_tropes.get_links('Main/BadSanta')
+        links = parse_html.get_links('Main/BadSanta')
         self.assertEqual(len(links), 305)
 
     def test_trope_with_subpages(self):
         '''
         Parsing a Trope Page with subpages
         '''
-        links = scraping.parse_media_for_tropes.get_links('Main/ActionGirl')
+        links = parse_html.get_links('Main/ActionGirl')
         self.assertEqual(len(links), 1853)
-
-# print(links)
-# print(len(links))
-
-# scraping.parse_media_for_tropes.HTML_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
-# passed = len(links) == 
-# print('Trope page with subpages: {}'.format('passed' if passed else 'failed'))
-# print(links)
-# print(len(links))
 
 if __name__ == '__main__':
     unittest.main()
