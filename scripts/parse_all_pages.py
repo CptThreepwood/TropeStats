b6625513eb028e9f2a25a2d2eb00e094dd48c41d
David Jennens <d.jennens@gmail.com>
2021-03-14 17:36:41 +1100

    tweaking logging

--- scripts/parse_all_pages.py
+++ scripts/parse_all_pages.py
@@ -16,9 +16,10 @@ console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.DEBUG)
 console_handler.addFilter(lambda record: record.levelno == logging.INFO)
 
-logging.basicConfig(
-    handlers=[file_handler, console_handler]
-)
+logger = logging.getLogger('ParseAllLog')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 ## Find all the pages we've got downloaded
 articles = glob.glob(os.path.join(HTML_DIR, '*', '*.html'))
@@ -31,32 +32,32 @@ for article in articles:
     article_check = parse.article_from_url(article)
 
     ## Check if we know what this file should be
-    if not (is_ignored(article_check) or is_media(article_check) or is_trope(article_check)):
-        logging.warning('Unknown Page Type: ', article)
+    if not (parse.is_ignored(article_check) or parse.is_media(article_check) or parse.is_trope(article_check)):
+        logger.warning('Unknown Page Type: {0}'.format(article))
         continue
 
     ## Ignore pages we've already processed or we're deliberately ignoring
-    if os.path.exists(outname) or is_ignored(article_check):
+    if os.path.exists(outname) or parse.is_ignored(article_check):
         continue
 
     ## Create output dir if it doesn't exist
     if not os.path.exists(os.path.dirname(outname)):
         os.makedirs(os.path.dirname(outname))
 
-    logging.info('Parsing {}'.format(article))
+    logger.info('Parsing {}'.format(article))
     try:
-        bigraph_links = get_links(article)
+        bigraph_links = parse.get_links(article)
         with open(outname, 'w') as out:
-            logging.info('Success:', outname)
+            logger.info('Success {}'.format(outname))
             successes += 1
             yaml.dump(bigraph_links, out)
-    except MissingArticlesError as err:
+    except parse.MissingArticlesError as err:
         failures += 1
-        logging.info('Failed')
-        logging.error('Failed to parse {}'.format(article))
+        logger.info('Failed')
+        logger.error('Failed to parse {}'.format(article))
         missing += len(err.articles)
         for article in articles:
-            logging.error('No such file {0}'.format(article)
+            logger.error('No such file {0}'.format(article))
 
-logging.info('Successfully parsed {0} pages'.format(successes))
-logging.info('Missing {0} files required for {1} pages'.format(missing, failures))
+logger.info('Successfully parsed {0} pages'.format(successes))
+logger.info('Missing {0} files required for {1} pages'.format(missing, failures))
