18483d4de5553a99cfc282ec21bc448debdc7116
David Jennens <d.jennens@gmail.com>
2021-03-14 16:30:43 +1100

    Don't bother to process ignore indices

--- scripts/parse_all_pages.py
+++ scripts/parse_all_pages.py
@@ -5,7 +5,7 @@ import time
 import logging
 
 from config import HTML_DIR, PARSED_DIR
-from parsing.parse_html import get_links
+from parsing.parse_html import get_links, is_ignored, article_from_url
 
 logging.basicConfig(
     filename='logs/parse_html_{}.log'.format(time.time()),
@@ -17,7 +17,7 @@ failures = 0
 missing = 0
 for article in articles:
     outname = os.path.splitext(article.replace(HTML_DIR, PARSED_DIR))[0] + '.yaml'
-    if os.path.exists(outname):
+    if os.path.exists(outname) or is_ignored(article_from_url(article)):
         continue
     if not os.path.exists(os.path.dirname(outname)):
         os.makedirs(os.path.dirname(outname))
