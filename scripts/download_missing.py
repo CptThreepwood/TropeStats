21209534503e1d0236817e5b5f3ffe4412b3270a
David Jennens <d.jennens@gmail.com>
2021-03-17 19:22:27 +1100

    Refactor downloader for more downloading scripts

--- scripts/download_missing.py
+++ scripts/download_missing.py
@@ -4,37 +4,17 @@ import time
 import requests
 
 from config import ROOT_DIR, HTML_DIR
+from scraping.downloader import download_and_store
+import parsing.parse_html as parse
 
 # ERROR:root:[Errno 2] No such file or directory: '/home/ubuntu/TropeStats/data/raw/BerserkButton/AnimeAndManga.html'
 
-TVTROPES_BASE = 'http://tvtropes.org/pmwiki/pmwiki.php/'
-
-def download_url(url, wait=2):
-    try:
-        return requests.get(url, timeout=10).content
-    except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
-        print('timed out, retrying')
-        time.sleep(wait)
-        return download_url(url, wait*2)
-
-def get_url(line):
-    article = os.path.join(*line.split()[-1].strip("'").split('/')[-2:])
-    return article, TVTROPES_BASE + os.path.splitext(article)[0]
+def get_article(line):
+    return parse.article_from_url(line.split()[-1])
 
 def get_latest_log():
     logs = glob.glob(os.path.join(ROOT_DIR, 'logs', 'parse_html*.log'))
     return sorted(logs)[-1]
-
-def download_and_store(line):
-    article, url = get_url(line)
-    outfile = os.path.join(HTML_DIR, article)
-    if os.path.exists(outfile):
-        return
-    if not os.path.exists(os.path.dirname(outfile)):
-        os.makedirs(os.path.dirname(outfile))
-    print(outfile)
-    with open(outfile, 'wb') as out_io:
-        out_io.write(download_url(url))
 
 if __name__ == '__main__':
     log_file = get_latest_log()
@@ -42,4 +22,3 @@ if __name__ == '__main__':
         for line in log_io.readlines():
             if 'No such file' in line:
                 download_and_store(line)
-
