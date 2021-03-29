2b88afeef02b217b3940830d94ec8aa10576d6fa
David Jennens <d.jennens@gmail.com>
2021-03-29 23:44:33 +1100

    Clear invalid data (optional)

--- scripts/data_health_checks.py
+++ scripts/data_health_checks.py
@@ -4,11 +4,14 @@ import glob
 from constants import PARSED_DIR
 from utils import get_matching_articles, load_parsed
 
-def find_empty():
+def find_empty(clear=False):
     parsed_data = get_matching_articles(os.path.join(PARSED_DIR, '*/*.yaml'))
     for p in parsed_data:
         if len(load_parsed(p)) == 0:
             print(p, 'contains no data')
+            if clear:
+                print('Deleting...')
+                os.remove(os.path.join(PARSED_DIR, p.to_string()))
 
 if __name__ == '__main__':
-    find_empty()
+    find_empty(clear=True)
